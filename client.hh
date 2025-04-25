1fd662875185556dfdaa34b06183b08988f6d8fb
isaiah-barron <isaiahdbarron@gmail.com>
2025-04-24 17:46:28 -0700

    client header file

---
+++ client.hh
@@ -0,0 +1,41 @@
+#ifndef CLIENT_HH
+#define CLIENT_HH
+
+#include <netinet/in.h>
+
+const int PORT = 8080;
+
+class Client{
+public:
+    // Setup client state
+    Client(ssize_t buf_size); //Constructor
+    ~Client(); //Deconstructor
+    
+    void connect_to_server();
+    void run();
+    void close_client_socket();
+
+private:
+    // Static pointer to the current instance
+    // we need this so we can call non-static 
+    // methods in a static method
+    static Client* instance;
+
+    char *buffer;
+    ssize_t BUFFER_SIZE;
+    int socket_fd;
+    struct sockaddr_in server_address;
+
+    // signale handling
+    static void signal_handler(int s);
+    void create_signal_handling();
+
+    // client messaging
+    int read_user_input();
+    void resize_buffer();
+    void send_message_length(ssize_t data_len);
+    ssize_t send_message(const void *buf, size_t n);
+    void send_all_data(ssize_t data_written);
+};
+
+#endif

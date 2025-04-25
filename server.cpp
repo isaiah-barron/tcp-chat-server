8f93fb9030c2ba58dbbdb796891338b30025dd25
isaiah-barron <isaiahdbarron@gmail.com>
2025-04-25 01:48:14 -0700

    update server.cpp and server.hh

---
+++ server.cpp
@@ -0,0 +1,9 @@
+#include "client.hh"  // Include the header file to use class declarations
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+using namespace std;
--- server.hh
+++ server.hh
@@ -4,4 +4,20 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+class Server{
+public:
+
+    //constructor desctructor
+    Server(ssize_t buff_size);
+    ~Server();
+
+private:
+
+    char *buffer;
+    ssize_t BUFFER_SIZE;
+    int conn_fd;
+
+    
+};
+
 #endif // SERVER.hh
